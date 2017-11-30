ad49e517ad2b511811c3d7100488c86320f09ad3
Arne Welzel <arne.welzel@gmail.com>
2017-11-30 12:52:34 +0600

    tourmap: make app name configurable for presentation purposes
    ...and some love for titles.

--- tourmap/config/defaults.py
+++ tourmap/config/defaults.py
@@ -1,6 +1,9 @@
 """
 # Set a few default options that are needed.
 """
+# Used in templates so we do not have to hard-code this...
+APP_NAME = "tourmapp"
+
 # How to access Strava
 STRAVA_OAUTH_AUTHORIZE_URL = "https://www.strava.com/oauth/authorize"
 STRAVA_OAUTH_TOKEN_URL = "https://www.strava.com/oauth/token"
@@ -11,4 +14,5 @@ HASHIDS_MIN_LENGTH = 8
 # New default for Flask-SQLAlchemy
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+# For environment checks???
 HEROKU = False
