21951137228f474362fdeadfc3acb3a4e0317fd5
Arne Welzel <arne.welzel@gmail.com>
2017-11-30 19:04:38 +0600

    tourmap: rework app.py/create_app() for testing

---
+++ tourmap/__init__.py
@@ -0,0 +1,123 @@
+import os
+import logging
+
+import click
+
+from flask import Flask, render_template, abort
+# from flask.logging import default_handler
+
+from flask_assets import Environment, Bundle
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def _is_heroku_env(environ=None):
+    environ = os.environ if not environ else environ
+    return "DYNO" in environ and "heroku" in os.environ.get("PATH", "")
+
+
+def configure_app(app, config):
+    """
+    Load the configuration for this app.
+
+    Developing locally should be easy by using CONFIG_PYFILE and loading
+    a local file. At the same time, we want to easily support heroku by
+    just fetching everything from the environment.
+
+    :param config: Set the app.config from this dictionary and do not
+        try any other methods of configuration. This should be used for
+        testing...
+    """
+    app.config.from_object("tourmap.config.defaults")
+
+    if config:
+        logger.info("Explicit config dict provided...")
+        app.config.update(config)
+    elif _is_heroku_env():
+        logger.info("Detected heroku environment...")
+        app.config.from_object("tourmap.config.heroku")
+    else:
+        config_pyfile = os.environ.get("CONFIG_PYFILE", "../config.py")
+        logger.info("Reading local config %s...", config_pyfile)
+        app.config.from_pyfile(config_pyfile)
+
+    # SQLAlchemy configuration...
+    from tourmap import database
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config["DATABASE_URL"]
+    database.db.init_app(app)
+
+    return app
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+    # Load default configuration that is with the application
+    app = configure_app(app, config=config)
+
+    # Static assets...
+    assets = Environment(app)
+    bundles = {
+        "base_js": Bundle(
+            "js/lib/jquery-3.2.1.js",
+            "js/lib/bootstrap-3.3.7.js",
+            "js/tourmap.js",
+            output="gen/tourmap.js"
+        ),
+        "css": Bundle(
+            "css/lib/bootstrap-3.3.7.css",
+            "css/lib/bootstrap-theme-3.3.7.css",
+            "css/tourmap.css",
+            output="gen/tourmap.css"
+        ),
+        "map_js": Bundle(
+            "js/lib/leaflet-1.2.0.js",
+            output="gen/map.js"
+        ),
+        "map_css": Bundle(
+            "css/lib/leaflet-1.2.0.css",
+            output="gen/map.css"
+        ),
+    }
+    assets.register(bundles)
+
+    @app.cli.command()
+    def createdb():
+        database.db.metadata.create_all(
+            bind=database.db.engine,
+            checkfirst=True
+        )
+
+    @app.cli.command()
+    @click.argument("user_id", type=click.INT)
+    def sync_activities(user_id):
+        from tourmap import tasks
+        tasks.sync_activities(user_id, environ=app.config)
+
+    @app.cli.command()
+    def resetdb():
+        database.db.drop_all()
+        database.db.create_all()
+
+    @app.cli.command()
+    def iem():
+        from tourmap.database import Activity, ActivityPhoto, Tour, User
+        import IPython
+        IPython.embed()
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
+
+    # Install a view views...
+    from tourmap.views import strava
+    app.register_blueprint(strava.create_blueprint(app), url_prefix="/strava")
+    from tourmap.views import users
+    app.register_blueprint(users.create_blueprint(app), url_prefix="/users")
+
+    return app
