1e111afcdcf2a41c7732edf5dde3caf033860fe8
Arne Welzel <arne.welzel@gmail.com>
2017-12-03 17:15:27 +0600

    tourmap/utils: add object pool implementation

---
+++ tourmap/utils/objpool.py
@@ -0,0 +1,106 @@
+"""
+A very thin wrapper around queue.LifoQueue providing
+thread safe object pool functionality.
+"""
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+
+class Empty(queue.Empty):
+    """objpool.Empty is raised when block and/or timeout are set."""
+    pass
+
+
+_PlaceHolder = object()
+
+
+class ObjectPool(object):
+
+    def __init__(self, cfn, maxsize=0):
+
+        self.__cfn = cfn
+        self.__pool = queue.LifoQueue(maxsize=maxsize)
+        self.__maxsize = maxsize
+
+        # Pre-populate the pool with None objects if a max_size
+        # is provided - this allows us to construct new objects
+        # without the need of keeping a counter how many we have
+        # already constructed...
+        if self.__maxsize:
+            for _ in range(self.__maxsize):
+                self.__pool.put_nowait(_PlaceHolder)
+
+    def get(self, block=True, timeout=None):
+        return self._get(block=block, timeout=timeout)
+
+    def put(self, obj):
+        return self._put(obj)
+
+    def use(self, block=True, timeout=None):
+        """
+        For use with the with statement.
+
+            with pool.use() as client:
+                response = client.request("/users/123")
+        """
+        class _CtxManager(object):
+            def __init__(self, obj, pool):
+                self.__pool = pool
+                self.__obj = obj
+
+            def __enter__(self):
+                return self.__obj
+
+            def __exit__(self, exc_type, exc_value, traceback):
+                self.__pool.put(self.__obj)
+
+        obj = self.get(block=block, timeout=timeout)
+        return _CtxManager(obj, self.__pool)
+
+    def size(self):
+        """
+        Just forward to the underlying LifoQueue. The size may change
+        at anytime.
+        """
+        return self.__pool.qsize()
+
+    def _get(self, block=True, timeout=None):
+        """
+        Get an instance from this pool, optionally constructing one
+        if the pool is empty and maxsize has not been reached yet.
+
+        :param block: as for queue.Queue. Note, a pool with an unset
+                      maxsize never blocks. It will always construct
+                      a new object for you.
+        :param timeout: How long to wait in the case of an empty pool.
+                        None will wait forever.
+        """
+        try:
+            obj = self.__pool.get_nowait()
+            # We are allowed to create a new instance if the pool
+            # returned the PlaceHolder object without any checks.
+            if obj is _PlaceHolder:
+                return self.__cfn()
+            return obj
+
+        except queue.Empty:
+            # If the pool was empty and no maxsize is set, we can
+            # also just create a new instance.
+            if not self.__maxsize:
+                return self.__cfn()
+
+        # In all other cases, we have to wait for someone else to
+        # put back a previously created instance.
+        try:
+            return self.__pool.get(block=block, timeout=timeout)
+        except queue.Empty:
+            raise Empty()
+
+    def _put(self, obj):
+        """
+        Put an object back into this pool. This may raise queue.Full,
+        but then the user screwed up.
+        """
+        self.__pool.put_nowait(obj)
---
+++ tourmap_test/test_objpool.py
@@ -0,0 +1,83 @@
+import queue
+import unittest
+
+from tourmap.utils import objpool
+
+class NumberMaker(object):
+
+    def __init__(self):
+        self.counter = 0
+
+    def cfn(self):
+        self.counter += 1
+        return self.counter
+
+class TestObjectPool(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_pool_construction_with_maxsize(self):
+        pool = objpool.ObjectPool(NumberMaker().cfn, maxsize=3)
+        self.assertEqual(1, pool.get())
+        self.assertEqual(2, pool.get())
+        self.assertEqual(3, pool.get())
+        with self.assertRaises(objpool.Empty):
+            pool.get(timeout=0.01)
+
+    def test_pool_construction_without_maxsize(self):
+        pool = objpool.ObjectPool(NumberMaker().cfn)
+        for x in range(1, 100):
+            self.assertEqual(x, pool.get())
+
+    def test_pool_construction_get_put_sequences(self):
+        pool = objpool.ObjectPool(NumberMaker().cfn)
+        for x in range(1, 100):
+            self.assertEqual(x, pool.get())
+        for x in range(1, 100):
+            pool.put(x)
+        self.assertEqual(99, pool.size())
+
+    def test_pool_construction_just_one(self):
+        pool = objpool.ObjectPool(NumberMaker().cfn)
+        for _ in range(1, 100):
+            obj = pool.get()
+            self.assertEqual(1, obj)
+            pool.put(obj)
+        self.assertEqual(1, pool.size())
+
+    def test_pool_is_lifo_style(self):
+        pool = objpool.ObjectPool(NumberMaker().cfn, maxsize=2)
+        self.assertEqual(1, pool.get())
+        self.assertEqual(2, pool.get())
+        pool.put(1)
+        pool.put(2)
+        self.assertEqual(2, pool.get())
+
+    def test_pool_error_when_using_put_on_full_pool(self):
+        pool = objpool.ObjectPool(NumberMaker().cfn, maxsize=2)
+        with self.assertRaises(queue.Full):
+            pool.put(objpool._PlaceHolder)
+
+    def test_ctx_manager(self):
+        pool = objpool.ObjectPool(NumberMaker().cfn, maxsize=2)
+
+        with pool.use() as obj1:
+            self.assertEqual(1, obj1)
+
+        with pool.use() as obj1:
+            self.assertEqual(1, obj1)
+
+        with pool.use() as obj1:
+            self.assertEqual(1, obj1)
+            with pool.use() as obj2:
+                self.assertEqual(2, obj2)
+
+        with pool.use() as obj1:
+            self.assertEqual(1, obj1)
+
+        with pool.use() as obj1:
+            with pool.use() as obj2:
+                with self.assertRaises(objpool.Empty):
+                    with pool.use(block=False) as obj3:
+                        pass
